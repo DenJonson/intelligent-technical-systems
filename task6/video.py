10fc5323cd39ac336e41c3685e1359df04671180
Den Jonson <denis.alescin23@gmail.com>
2022-11-27 18:21:16 +0300

    Модификация программы
    При нажатии на видео будет появляться квадрат черного цвета в месте нажатия на левую кнопку мыши

--- task6/video.py
+++ task6/video.py
@@ -2,13 +2,26 @@ import cv2 as cv
 
 #Get object from camera
 capture = cv.VideoCapture(0)
+class Cords:
+  rec_x = 0
+  rec_y = 0
+
+def show_clicked(event, x, y, flags, param):
+  if event == cv.EVENT_LBUTTONDOWN:
+    param.rec_y = y
+    param.rec_x = x
+    
+
+
 
 #inf loop for video
 while(1):
   #read frame from camera
   ret, frame = capture.read()
+  cv.rectangle(frame, (Cords.rec_x, Cords.rec_y), (Cords.rec_x + 100, Cords.rec_y + 100), (0, 0, 0), thickness=3)
   #display the frame
   cv.imshow('Camera', frame)
+  cv.setMouseCallback('Camera', show_clicked, param=Cords)
   #Quit from loop if "q" key was pressed
   if cv.waitKey(1) & 0xFF == ord('q'):
     break
