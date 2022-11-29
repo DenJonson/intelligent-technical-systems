894502d51bd59b9d4860f7a8aa08bdcc9fe98e74
DenJonson <denis.alescin23@gmail.com>
2022-11-29 10:39:46 +0300

    Success

--- task6/video.py
+++ task6/video.py
@@ -1,17 +1,15 @@
 import cv2 as cv
 import numpy as np
 
-#Get object from camera
-capture = cv.VideoCapture(0)
-set_ret, set_frame = capture.read()
-
 class Mask:
-  rectangles = np.ones(set_frame.shape, dtype = set_frame.dtype) * 0
+  rectangles = np.zeros((480, 640, 1), dtype = "uint8")
 
 def show_clicked(event, x, y, flags, param):
   if event == cv.EVENT_LBUTTONDOWN:
-    cv.rectangle(param.rectangles, (x, y), (x+50, y+50), (0,255,0), thickness=2)
+    cv.rectangle(param.rectangles, (x, y), (x+50, y+50), (255), thickness=2)
+    print("Click success")
     
+capture = cv.VideoCapture(0)
 
 #inf loop for video
 while(1):
@@ -20,7 +18,10 @@ while(1):
   #display the frame
   cv.namedWindow(winname='Camera')
   cv.setMouseCallback('Camera', show_clicked, param=Mask)
-  frame = cv.merge(frame, Mask.rectangles)
+  
+  con, hir = cv.findContours(Mask.rectangles, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+  
+  cv.drawContours(frame, con, -1, (0,255,0), 1)
   cv.imshow('Camera', frame)
   #Quit from loop if "q" key was pressed
   if cv.waitKey(1) & 0xFF == ord('q'):
