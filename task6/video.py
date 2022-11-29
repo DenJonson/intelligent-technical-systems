cab63a8c5266722cc6743b83b8d6d1e3c38c5973
Den Jonson <denis.alescin23@gmail.com>
2022-11-29 09:17:05 +0300

    .

--- task6/video.py
+++ task6/video.py
@@ -1,27 +1,27 @@
 import cv2 as cv
+import numpy as np
 
 #Get object from camera
 capture = cv.VideoCapture(0)
-class Cords:
-  rec_x = 0
-  rec_y = 0
+set_ret, set_frame = capture.read()
+
+class Mask:
+  rectangles = np.ones(set_frame.shape, dtype = set_frame.dtype) * 0
 
 def show_clicked(event, x, y, flags, param):
   if event == cv.EVENT_LBUTTONDOWN:
-    param.rec_y = y
-    param.rec_x = x
+    cv.rectangle(param.rectangles, (x, y), (x+50, y+50), (0,255,0), thickness=2)
     
-
-
 
 #inf loop for video
 while(1):
   #read frame from camera
   ret, frame = capture.read()
-  cv.rectangle(frame, (Cords.rec_x, Cords.rec_y), (Cords.rec_x + 100, Cords.rec_y + 100), (0, 0, 0), thickness=3)
   #display the frame
+  cv.namedWindow(winname='Camera')
+  cv.setMouseCallback('Camera', show_clicked, param=Mask)
+  frame = cv.merge(frame, Mask.rectangles)
   cv.imshow('Camera', frame)
-  cv.setMouseCallback('Camera', show_clicked, param=Cords)
   #Quit from loop if "q" key was pressed
   if cv.waitKey(1) & 0xFF == ord('q'):
     break
