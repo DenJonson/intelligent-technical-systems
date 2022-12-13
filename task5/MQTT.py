cd4a7ccddba969eb221cb60995a3135e9dc7d948
DenJonson <denis.alescin23@gmail.com>
2022-12-13 12:00:21 +0300

    Налажена связь с брокером

--- task5/MQTT.py
+++ task5/MQTT.py
@@ -1,15 +1,49 @@
-import paho.mqtt.client as mqtt
+import random
+import time
 
-def on_connect(client, userdata, flags, rc):
-  print("Connected with result code {O}".format(str(rc)))
-  client.subscribe("digitest/test1")
-  
-def on_message(client, userdata, msg):
-  print("Massage received-> " + msg.topic + " " + str(msg.payload))
-  
-client = mqtt.Client("digi_mqtt_test")
-client.on_connect = on_connect
-client.on_message = on_message
+from paho.mqtt import client as mqtt_client
 
-client.connect('127.0.0.1', 17300)
-client.loop_forever()
+
+broker = 'srv1.clusterfly.ru'
+port = 9124
+topic = "/python/mqtt"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def publish(client):
+    msg_count = 0
+    while True:
+        time.sleep(1)
+        msg = f"messages: {msg_count}"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+
+
+if __name__ == '__main__':
+    run()
